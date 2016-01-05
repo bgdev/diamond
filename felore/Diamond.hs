e1e82b9bf1509eb8fc140dc7343375d6e5461f12
Yordan Miladinov <yordan.miladinov@chaosgroup.com>
2016-01-05 20:18:07 +0000

    stuff

--- felore/Diamond.hs
+++ felore/Diamond.hs
@@ -4,17 +4,15 @@ import Data.Char
 
 diamond = map mirror . mirror . edge
 
-dist a b = (ord a) - (ord b)
-
 edge x = map (pad x) ['A' .. x]
 
 mirror xs = xs ++ (tail $ reverse xs)
 
 pad stop x = (spaces pre) ++ [x] ++ (spaces post)
-  where len = dist stop 'A'
-        post = dist x 'A'
-        pre = len - post
-
-spaces x = take x $ repeat ' '
+  where len      = dist stop 'A'
+        post     = dist x 'A'
+        pre      = len - post
+        dist a b = (ord a) - (ord b)
+        spaces x = take x $ repeat ' '
 
 main = mapM_ putStrLn $ diamond 'H'
